081cf864f195f613532f0a6f5ec072994ff020a6
DriesC-1850394 <dries_cardinaels@student.uhasselt.be>
2022-04-13 19:06:23 +0200

    :lipstick: Implemented First Step UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [index, setIndex] = useState<number>(0)
+  const [names, setNames] = useState<Array<string>>([])
+  const [textAreaText, setTextAreaText] = useState<string>("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Title">Worksheet Generator</div>
+      {index === 0 &&
+        <div className="FirstStep">
+          <div className="StepText">ENTER NAMES</div>
+          <textarea name="description" placeholder='enter names...' onChange={e => setTextAreaText(e.target.value)} />
+          <button className="NextStepButton" onClick={() => enter()}>Next</button>
+        </div>
+      }
     </div>
   );
+
+
+  function enter() {
+
+  }
 }
 
 export default App;
